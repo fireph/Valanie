fcfc52bb43055aa40b23a335b1aeb214bbc90dcf
DungFu <dungfu3@gmail.com>
2014-02-13 21:46:17 -0800

    initial commit with basic game done

---
+++ glasses.js
@@ -0,0 +1,43 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "Glasses.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 11252,
+        "faces"         : 11152,
+        "normals"       : 0,
+        "uvs"           : 0,
+        "materials"     : 2
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "Plastic",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.003095, 0.003095, 0.003095],
+	"colorSpecular" : [1.0, 1.0, 1.0],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 915.686275,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "Glass",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.64, 0.64, 0.64],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 0.085
+	}],
+
+    "buffers": "glasses.bin"
+
+}
---
+++ nightstand.js
@@ -0,0 +1,58 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "Nightstand.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 670,
+        "faces"         : 654,
+        "normals"       : 0,
+        "uvs"           : 789,
+        "materials"     : 3
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "Wood",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.64, 0.64, 0.64],
+	"colorSpecular" : [0.5, 0.5, 0.5],
+	"illumination" : 2,
+	"mapDiffuse" : "Wood2.jpg",
+	"opticalDensity" : 1.0,
+	"specularCoef" : 96.078431,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "Plastic",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.058586, 0.058586, 0.058586],
+	"colorSpecular" : [0.513699, 0.513699, 0.513699],
+	"illumination" : 2,
+	"opticalDensity" : 1.0,
+	"specularCoef" : 194.117647,
+	"transparency" : 1.0
+	},
+
+	{
+	"DbgColor" : 60928,
+	"DbgIndex" : 2,
+	"DbgName" : "Alu",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.505983, 0.504709, 0.585569],
+	"colorSpecular" : [0.520548, 0.520548, 0.520548],
+	"illumination" : 2,
+	"mapDiffuse" : "Metal.jpg",
+	"opticalDensity" : 1.0,
+	"specularCoef" : 258.823529,
+	"transparency" : 1.0
+	}],
+
+    "buffers": "nightstand.bin"
+
+}
